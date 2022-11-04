57a69e373c4cea96691e1e3be3da3101178198c7
Mohammadreza Molaei <mrmolaei.ir@gmail.com>
2022-11-04 14:47:55 +0330

    Add tabs to options page

--- inc/Callbacks/GeneralOptions.php
+++ inc/Callbacks/GeneralOptions.php
@@ -10,9 +10,8 @@ class GeneralOptions
 	{
 //		update_option( 'todoit_options', array('general_features' => 'basic') );
 		$value = get_option('todoit_options')['general_features'];
-		echo "<div class='o-row'>";
-		echo "<label class='o-col o-col--1/2'><input ". ($value == 'basic' ? 'checked' : '') ." type='radio' value='basic' name='todoit_options[general_features]' /> Basic</label>";
-		echo "<label class='o-col o-col--1/2'><input ". ($value == 'pro' ? 'checked' : '') ." type='radio' value='pro' name='todoit_options[general_features]' /> Professional</label>";
-		echo "</div>";
+		echo "<label'><input ". ($value == 'basic' ? 'checked' : '') ." type='radio' value='basic' name='todoit_options[general_features]' /> Basic</label>";
+		echo "<div class='o-spacer o-spacer--2x'></div>";
+		echo "<label class='disabled'><input ". ($value == 'pro' ? 'checked' : '') ." disabled type='radio' value='pro' name='todoit_options[general_features]' /> Professional (Coming soon)</label>";
 	}
 }
--- templates/options/general.php
+++ templates/options/general.php
@@ -1,14 +1,32 @@
 <?php settings_errors(); ?>
 <div class="todo-it">
-    <div class="o-wrap">
-        <div class="o-col--1/2">
-            <form method="post" action="options.php">
-		        <?php
-		        settings_fields( 'todoit_options' );
-		        do_settings_sections( 'todoit_options_page' );
-		        submit_button();
-		        ?>
-            </form>
+    <div class="c-tabs c-tabs--no-border js-tabs">
+        <div class="c-tabs__nav">
+            <ul class="c-tabs__nav__list">
+                <li class="c-tabs__nav__item">
+                    <a href="#general" class="c-tabs__link js-tab-link is-active">General</a>
+                </li>
+                <li class="c-tabs__nav__item">
+                    <a href="#customization" class="c-tabs__link js-tab-link">Customization</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="c-tabs__pane">
+            <div id="general" class="c-tabs__pane__tab js-tabs-tab is-active">
+                <div class="o-col--1/2">
+                    <form method="post" action="options.php">
+			            <?php
+			            settings_fields( 'todoit_options' );
+			            do_settings_sections( 'todoit_options_page' );
+			            submit_button();
+			            ?>
+                    </form>
+                </div>
+            </div>
+            <div id="customization" class="c-tabs__pane__tab js-tabs-tab">
+                <h2>Customization</h2>
+            </div>
         </div>
     </div>
 </div>
